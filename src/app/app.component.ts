fe750a5098fbc53dbfe9066e015eaf37cc2d478e
masakiJapan <masaki8766@gmail.com>
2023-02-13 20:56:12 +0900

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; // コンポーネントの定義に必要なオブジェクト（モジュール）
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  // クラス定義だけだとコンポーネントとして認識されない
+  // @ComponentデコレーターでAngularのコンポーネントとして定義
+  selector: 'app-root', // このコンポーネントを適用すべき要素
+  //templateUrl: './app.component.html', // コンポーネントに適用するビュー。template で直接htmlを記述することも出来る。
+  template: `<h1>Hello {{name}}</h1><p>{{now | date:'short':'JP'}}</p>`, // {{...}} はプレースフォルダ。
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // クラス定義
+  // コンポーネントの本体（データ、振る舞い）
   title = 'my-app';
+  name = "(´・ω・｀)"; // ビュー変数。コンポーネントのプロパティ。
+  now: string = "2023/02/12"
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+// ルートモジュール：起動時に呼び出されるモジュール ≒ メインモジュール 
 
-import { AppComponent } from './app.component';
+// import でオブジェクト()をインポートする。
+// import {name, ...} from module の構文。モジュールの実態は TypeScript。
+import { NgModule } from '@angular/core'; // モジュールを定義するために必要なモジュール。@NgModuleで使う。@angular は angular の標準モジュール。
+import { BrowserModule } from '@angular/platform-browser'; // アプリをブラウザで動作させるために必要なモジュール。
 
-@NgModule({
+import { AppComponent } from './app.component'; // アプリを構成するコンポーネント。
+import { XyzComponent } from './xyz/xyz.component';
+
+@NgModule({ // @NgModuleデコレーター
   declarations: [
-    AppComponent
+    // このモジュールで使うコンポーネント。コンポーネント＝ページを構成するUI部品。
+    AppComponent, // パラメータ名
+    XyzComponent
   ],
   imports: [
+    // このモジュールで使う他のモジュール
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] // 最上位のコンポーネントを指定
 })
 export class AppModule { }
